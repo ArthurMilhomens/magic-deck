9850a518ad55e50428cbff5eaf33b6a167233d7f
Arthur Oliveira <arthur.oliveira@grupoicts.com.br>
2020-12-09 09:49:22 -0300

    changing base api to scryfall api

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Header from './Header';
 import SideMenu from './SideMenu';
 
 import { Container } from './styles';
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,7 @@ import magic from '../../services/magic';
 function Home() {
 
   useEffect(() => {
-    magic.get('/cards?name=nicol bolas')
+    magic.get('/cards?id=[cd236ea8-c7a6-5b66-8190-996c09f8c57e,4ce865a1-1518-5629-97a8-ac80dc2e9230]')
       .then(res => {
         console.log(res)
       })
